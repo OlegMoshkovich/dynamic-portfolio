8379da4c39072adfa8dfe1457609d1be2e5d26ac
Oleg Moshkovich <oleg.mosh@gmail.com>
2018-11-09 18:05:07 -0500

    updated WTC

--- src/Components/Circle.js
+++ src/Components/Circle.js
@@ -18,7 +18,7 @@ class Circle extends Component{
     let widthSelected = this.props.width/35
     let circleItems = [];
     let type = this.props.type
-    console.log('the type in the circle component',type)
+
 
 
       for (var i = 1; i < this.props.quantity+1; i++) {
@@ -26,10 +26,8 @@ class Circle extends Component{
         if(i === parseInt(this.props.number)){
           circleItems.push(
                 <img alt= {`${i}`} key = {i}  className = "image" onClick ={(data,type) =>  this.props.updateParentComponent(data.target.alt,this.props.type)}  style = {{width:widthSelected, height:widthSelected,margin:2}}
-                src = {require(`../images/icons/circle_filled.png`)}/>
-);
+                src = {require(`../images/icons/circle_filled.png`)}/>);
         }else{
-          // console.log('in the else loop- number --',this.props.number)
           circleItems.push(
                 <img alt= {`${i}`}  key = {i} className = "image"  onClick ={(data,type) =>  this.props.updateParentComponent(data.target.alt,this.props.type)} style = {{width:width, height:width,margin:2}}
                 src = {require(`../images/icons/circle.png`)}/>
